aab9b6bf89f298faedf0d6e3e63d128fe03ccba5
Enieber Cunha <eniebercunha@gmail.com>
2017-12-14 08:32:57 -0300

    refactor fact test

--- fact/fact.test.js
+++ fact/fact.test.js
@@ -1,8 +1,7 @@
-import fact from './fact';
+const fact = require('./fact');
 
 describe('Factorial function', () => {
   it('should return 1 when input is 1', () => {
       expect(fact(1)).toBe(1);
     });
 });
-
