d0364f6c29338581a48050870f6ebb9f87a964c4
Enieber Cunha <eniebercunha@gmail.com>
2017-12-14 08:37:55 -0300

    refactoring yCombinatorFact

--- fact/yCombinatorFact.js
+++ fact/yCombinatorFact.js
@@ -1,2 +1,3 @@
 'use strict';
-export default (number) => ((fn) => fn(fn, number))((f, n) => (n <= 1)? 1: n * f(f, (n - 1)))
+
+module.exports = (number) => ((fn) => fn(fn, number))((f, n) => (n <= 1)? 1: n * f(f, (n - 1)))
